f17c99660d337680d141d3ab82e23e249088eea2
prktypes <olivercarshwell@gmail.com>
2025-01-21 19:19:06 +0530

    added an option to add tasks using the enter button

--- src/components/TodoInput.jsx
+++ src/components/TodoInput.jsx
@@ -3,15 +3,23 @@ import { useState } from "react"
 export function TodoInput(props){
     const { handleAddTodo } = props
     const [inputValue,setInputValue] = useState('')
+
+    const handleAddTask = () => {
+        if(!inputValue)return; // do nothing if input is empty
+        handleAddTodo(inputValue); // add the new task
+        setInputValue('');
+    }
     return(
         <div className="input-container">
             <input value={inputValue} onChange={(e)=>{
                 setInputValue(e.target.value)
-            }} placeholder="Add task"/>
+            }}
+            onKeyDown={(e)=>{
+                if(e.key==='Enter'){handleAddTask()}
+            }}
+            placeholder="Add task"/>
             <button onClick={()=>{
-                if(!inputValue){return}
-                handleAddTodo(inputValue)
-                setInputValue('')
+                handleAddTask()
             }}>
                 <i className="fa-solid fa-plus"></i>
             </button>
